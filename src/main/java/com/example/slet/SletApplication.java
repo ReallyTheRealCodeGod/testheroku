60587cb402c1284fe13ac6b918ef4084ce159ac9
Peter Bandsholm <lil.jdean@gmail.com>
2020-10-20 00:23:56 +0200

    first commit

---
+++ src/main/java/com/example/slet/SletApplication.java
@@ -0,0 +1,13 @@
+package com.example.slet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slet/SletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
